16fd0c8b83a288e273e41db33125c9d961cd6b4c
vipb <hiroflighter@gmail.com>
2019-04-14 23:50:41 +0900

    feat: cron and scrape 5ch

---
+++ src/bot/cron-job.js
@@ -0,0 +1,7 @@
+import { CronJob } from 'cron'
+
+export default robot => {
+  // new CronJob('0 30 18 * * 1-5', () => {
+  //   robot.send('#your_channel', '@here そろそろ帰る準備をしよう')
+  // }).start()
+}
---
+++ src/module/scrape.js
@@ -0,0 +1,38 @@
+import cheerio from 'cheerio'
+import { Iconv } from 'iconv'
+import axios from 'axios'
+const baseUrl = 'http://hebi.5ch.net/news4vip'
+const pageUrl = `${baseUrl}/subback.html`
+
+const sjis2utf8 = new Iconv('SHIFT_JIS', 'UTF-8//TRANSLIT//IGNORE')
+
+axios.defaults.responseType = 'arraybuffer'
+axios.defaults.transformResponse = [data => sjis2utf8.convert(data).toString()]
+
+async function getThreads() {
+  const res = await axios.get(pageUrl)
+  const $ = cheerio.load(res.data)
+  const threads = []
+  $('#trad > a').map((i, elA) => {
+    const a = $(elA)
+    const title = titleParse(a.text())
+    if (!title) {
+      return
+    }
+    const url = `${baseUrl}/${a.attr('href')}`
+    threads.push({ title, url })
+  })
+  return threads
+}
+
+function titleParse(text) {
+  const m = text.match(/^\d+: ([\s\S]*?) \((\d+)\)$/)
+  if (!m || !m[1]) {
+    return null
+  }
+  return m[1]
+}
+
+getThreads().then(threads => {
+  console.log({ threads })
+})
