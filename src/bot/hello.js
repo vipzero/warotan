78fe244cb97f22356f3b159f3677992d13452416
vipb <hiroflighter@gmail.com>
2019-04-15 11:44:22 +0900

    add stopped message

--- src/bot/hello.js
+++ src/bot/hello.js
@@ -1,5 +1,9 @@
 export default robot => {
-  robot.send({ room: 'vipbot_dev' }, 'やっはろー！起動したよ')
+  const say = robot.send.bind(null, { room: 'vipbot_dev' })
+  say('やっはろー！起動したよ')
+  process.on('SIGTERM', () => {
+    say('無課金ユーザなので寝ます(3:00〜9:00, heroku運用)')
+  })
   // TODO: get disconnected event then say "おやすみ"
   // robot.adapter.on 'disconnected' ??
 }
