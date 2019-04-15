c9f8100b38d7e4b25b70439b3701d5eb8e2b41ed
vipb <hiroflighter@gmail.com>
2019-04-15 13:44:39 +0900

    fix exit message

--- src/bot/hello.js
+++ src/bot/hello.js
@@ -1,9 +1,9 @@
+/* global process:false */
+
 export default robot => {
   const say = robot.send.bind(null, { room: 'vipbot_dev' })
   say('やっはろー！起動したよ')
-  process.on('SIGTERM', () => {
+  process.on('beforeExit', () => {
     say('無課金ユーザなので寝ます(3:00〜9:00, heroku運用)')
   })
-  // TODO: get disconnected event then say "おやすみ"
-  // robot.adapter.on 'disconnected' ??
 }
