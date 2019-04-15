ba80df845c31c90f12efba851199832a8c99d753
vipb <hiroflighter@gmail.com>
2019-04-15 13:47:29 +0900

    fix bind ;;

--- src/bot/hello.js
+++ src/bot/hello.js
@@ -1,7 +1,7 @@
 /* global process:false */
 
 export default robot => {
-  const say = robot.send.bind(null, { room: 'vipbot_dev' })
+  const say = message => robot.send({ room: 'vipbot_dev' }, message)
   say('やっはろー！起動したよ')
   process.on('beforeExit', () => {
     say('無課金ユーザなので寝ます(3:00〜9:00, heroku運用)')
