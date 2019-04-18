860d99c4e26f4b07671c4926f7d675185f2e536c
vipzero <hiroflighter@gmail.com>
2019-04-18 18:40:14 +0900

    Add welcome

--- src/bot/hello.js
+++ src/bot/hello.js
@@ -1,9 +1,18 @@
 /* global process:false */
+import { RTMClient } from '@slack/client'
 
 export default robot => {
   const say = message => robot.send({ room: 'vipbot_dev' }, message)
   say('やっはろー！起動したよ')
   process.on('beforeExit', () => {
     say('無課金ユーザなので寝ます(3:00〜9:00, heroku運用)')
+  })
+
+  const rtm = new RTMClient(robot.adapter.options.token)
+  rtm.on('member_joined_channel', async event => {
+    const reply = await rtm.sendMessage(
+      `@${event.user} ようこそ`,
+      event.channel
+    )
   })
 }
